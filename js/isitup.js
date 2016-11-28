258b27b9898d4006db46af5ee73a2155d618f668
Naveen Gurram <Naveen.Gurram@finra.org>
2016-11-28 15:54:17 -0500

    changes

--- js/isitup.js
+++ js/isitup.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     items.allowIndividualRetry = isItTrue(allowIndividualRetry);
     items.allowAutomaticRefresh = isItTrue(allowAutomaticRefresh);
     items.pushNotifications = isItTrue(pushNotifications);
-    items.pageRefreshAfter = isItTrue(pageRefreshAfter);
+    items.pageRefreshAfter = pageRefreshAfter;
     items.cfgTxt = cfgTxt;
     doProcess(items);
 
@@ -143,6 +143,7 @@ function checkHealth(spanIdMap, pushNotifications) {
                     jsonObj.url = value;
                     spanData = stringify(jsonObj);
                 }
+                $('#' + key).removeClass("status-code error");
                 $('#' + key).addClass("status-code success");
                 $('#' + key).attr('data', spanData);
                 $('#' + key).text("200");
